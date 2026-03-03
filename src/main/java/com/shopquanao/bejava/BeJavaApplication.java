6cd8f93c7f30cd1ffb73549503cde5e970650046
githubcopilot <maildemothoil@gmail.com>
2026-03-03 16:15:38 +0700

    init: Spring Boot project

---
+++ src/main/java/com/shopquanao/bejava/BeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.shopquanao.bejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopquanao/bejava/BeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopquanao.bejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
