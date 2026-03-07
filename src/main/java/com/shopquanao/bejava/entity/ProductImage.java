be30a518ed35620f939c03b94ee4eab9c2f55f4d
githubcopilot <maildemothoil@gmail.com>
2026-03-07 21:25:00 +0700

    thêm entity Product_Variants và Product_Images

---
+++ src/main/java/com/shopquanao/bejava/entity/ProductImage.java
@@ -0,0 +1,27 @@
+package com.shopquanao.bejava.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+// Entity ánh xạ với bảng "Product_Images" trong database
+@Entity
+@Table(name = "Product_Images")
+@Getter
+@Setter
+public class ProductImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private Integer imageId;
+
+    @Column(name = "product_id", nullable = false)
+    private Integer productId;
+
+    @Column(name = "image_url", nullable = false, length = 500)
+    private String imageUrl;
+
+    @Column(name = "is_main", nullable = false)
+    private Boolean isMain = false;
+}
---
+++ src/main/java/com/shopquanao/bejava/entity/ProductVariant.java
@@ -0,0 +1,30 @@
+package com.shopquanao.bejava.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+// Entity ánh xạ với bảng "Product_Variants" trong database
+@Entity
+@Table(name = "Product_Variants")
+@Getter
+@Setter
+public class ProductVariant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "variant_id")
+    private Integer variantId;
+
+    @Column(name = "product_id", nullable = false)
+    private Integer productId;
+
+    @Column(name = "color", length = 50)
+    private String color;
+
+    @Column(name = "size", length = 10)
+    private String size;
+
+    @Column(name = "stock_quantity", nullable = false)
+    private Integer stockQuantity = 0;
+}
